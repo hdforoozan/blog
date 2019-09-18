2997ab2b266f231a1bf27c869d746bba11eb1783
Hadi Foroozanfar <hdforoozan@gmail.com>
2019-09-18 12:54:15 +0430

    change database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogdb',
-        'USER': 'postgres',
-        'PASSWORD': 'hdlight74',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
